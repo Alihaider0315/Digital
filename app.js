03c34112e5f6268b1d29ff333afbf3e50767c586
Ali Haider <ah712200@Gmail.com>
2023-07-18 17:51:38 +0500

    Digital Clock Optimized

---
+++ app.js
@@ -0,0 +1,28 @@
+let stop;
+let startingclock = false;
+
+function mainClock(){
+    const recent = new Date();
+    let watch_hours = recent.getHours();
+    let watch_minut = recent.getMinutes();
+    let watch_secon = recent.getSeconds();
+    let watch_milse = String(recent.getMilliseconds()).padStart(3, '0').substring(0, 2);
+    const amPm = watch_hours >= 12 ? "PM" : "AM";
+    watch_hoursm = (watch_hours % 12) || 12 ;
+    document.getElementById('hours').textContent = `${watch_hours}:${watch_minut}:${watch_secon}:${String(watch_milse).padStart(2,'0')}:${amPm}`;
+}
+
+function startclock(){
+    if(startingclock){
+        clearInterval(stop);
+        document.getElementById('startstop').textContent = "Start";
+    }
+    else {
+        stop = setInterval(mainClock , 1000);
+        document.getElementById('startstop').textContent = "Stop";
+    }
+
+    startingclock = !startingclock;
+}
+
+document.getElementById('startstop').addEventListener('click' , startclock);
