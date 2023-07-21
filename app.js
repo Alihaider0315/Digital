77c8895f0035d186b344fcd71143aa4ac659e9cc
Alihaider0315 <ah712200@gmail.com>
2023-07-21 15:03:31 +0500

    day

--- app.js
+++ app.js
@@ -10,21 +10,31 @@ function mainClock(){
     const amPm = watch_hours >= 12 ? "PM" : "AM";
     watch_hours = (watch_hours % 12) || 12 ;
     document.getElementById('hours').textContent = `${watch_hours}:${watch_minut}:${watch_secon}:${String(watch_milse).padStart(2,'0')} ${amPm}`;
+    
+}
+
+
+function daytoday(){
+    const recent = new Date();
+    const dayToday = recent.getDay();
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const dayOfWeek = daysOfWeek[dayToday];
+    document.getElementById('today').textContent = `${dayOfWeek}`;
+    console.log(dayOfWeek);
 }
 
 function startclock(){
     if(startingclock){
         clearInterval(stop);
         document.getElementById('startstop').textContent = "Start";
-        button.classList.remove("red-button");
     }
     else {
         stop = setInterval(mainClock , 1000);
         document.getElementById('startstop').textContent = "Stop";
-        button.classList.add("red-button");
     }
 
     startingclock = !startingclock;
 }
 
 document.getElementById('startstop').addEventListener('click' , startclock);
+document.getElementById('today').textContent = `${dayOfWeek}`;
