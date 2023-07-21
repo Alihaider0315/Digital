c58c9b44eefca3a4af3ceb75edfa91c677dc3c5e
Alihaider0315 <ah712200@gmail.com>
2023-07-21 16:55:25 +0500

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function mainClock(){
     // let watch_milse = String(recent.getMilliseconds()).padStart(3, '0').substring(0, 2); // For Seconds
     const amPm = watch_hours >= 12 ? "PM" : "AM";
     watch_hours = (watch_hours % 12) || 12 ;
-    document.getElementById('hours').textContent = `${dayOfWeek}  ${watch_hours}:${watch_minut}:${watch_secon} ${amPm}`;
+    document.getElementById('hours').textContent = `${watch_hours}:${watch_minut}:${watch_secon} ${amPm}`;
     document.getElementById('today').textContent = `${dayOfWeek}`;
     
 }
